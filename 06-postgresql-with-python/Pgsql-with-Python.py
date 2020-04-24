5c3f313007c29185369233b2717e5354a882799d
maevadevs <maevadevs@users.noreply.github.com>
2020-04-24 12:55:52 -0500

    Review pgsql-with-python.py

--- 06-postgresql-with-python/Pgsql-with-Python.py
+++ 06-postgresql-with-python/Pgsql-with-Python.py
@@ -1,82 +1,47 @@
+## Using PostgreSQL in Python (with Psycopg2)
+ 
+## Problem: 
+## Your data is in a SQL database, but your machine learning tools are in Python.
 
-# coding: utf-8
+## Solution:
+## Run SQL queries from Python 
+# Very useful for scaled data pipelines, pre-cleaning, data exploration
+# Allows for dynamic query generation
 
-# # Using PostgreSQL in Python (with Psycopg2)
-# 
-# ## Problem: 
-# ### Your data is in a SQL database, but your machine learning tools are in Python.
-# 
-# ---------
-# 
-# ## Solution:
-# ### Run SQL queries from Python
-# 
-# * Very useful for scaled data pipelines, pre-cleaning, data exploration
-# * Allows for dynamic query generation
-# 
-# ---------
+## Psycopg2
+# A library that allows Python to connect to an existing PostgreSQL database to utilize SQL functionality 
 
-# ### Psycopg2
-# A library that allows Python to connect to an existing PostgreSQL database to utilize SQL functionality.
-# 
-# 
-# ### Documentation
-# * http://initd.org/psycopg/docs/install.html
+## Documentation
+# http://initd.org/psycopg/docs/install.html
 
-# # Objectives
-# 
+## Objectives 
 # - Learn how to connect to and run Postgres queries from Python
 # - Understand cursors, executes, and commits
 # - Learn how to generate dynamic queries
 
-# In[ ]:
-
-# basic usage
+# Basic usage
 query = "SELECT * FROM some_table;"
 cursor.execute(query)
 results = cursor.fetchall()
 
-
-# # Creating a connection with Postgres
-
-# ### Import
-
-# In[1]:
+# Creating a connection with Postgres
 
 import psycopg2 as pg2
 
-
-# ### Create connection with Postgres
-
-# In[2]:
-
+# Create connection with Postgres
 conn = pg2.connect(database='postgres', user='brad')
 
-
-# ### Retrieve the Cursor
-# 
-# * A cursor is a control structure that enables traversal over the records in a database.  You can think of it as an iterator or pointer for Sql data retrieval.
-
-# In[3]:
-
-
+# Retrieve the Cursor
+# A cursor is a control structure that enables traversal over the records in a database
+# You can think of it as an iterator or pointer for SQL data retrieval
 cur = conn.cursor()
 
-
-# ## Create a database
-
-# In[4]:
-
+# Create a database
 cur.execute('CREATE DATABASE lecture')
 
-
-# ## What happened?!
-# ### Normally we execute "temporary transactions", but database-wide operations cannot be run temporarily.
-# 
-# ### Try again:
-
-# In[6]:
-
+# What happened?!
+# Normally we execute "temporary transactions", but database-wide operations cannot be run temporarily
+# Try again:
 conn.close()
 
 conn = pg2.connect(dbname = 'postgres', user='brad')
@@ -85,180 +50,95 @@ conn.set_session(autocommit = True)
 cur = conn.cursor()
 cur.execute('CREATE DATABASE lecture')
 
-
-# ## Disconnect from the cursor and database (again)
-
-# In[7]:
-
+# Disconnect from the cursor and database (again)
 cur.close() # optional, closing connection always closes any associated cursors
 conn.close()
 
-
-# ## Let's use our new database
-
-# In[53]:
-
+# Now, let's use our newly created database
 conn = pg2.connect(database='lecture', user='brad')
-
-
-# In[54]:
-
 cur = conn.cursor()
 
-
-# ### Create a new table
-
-# In[55]:
-
+# Create a new table
 query1 = '''
-        CREATE TABLE logins (
-            userid integer
-            , tmstmp timestamp
-            , type varchar(10)
-        );
-        '''
-
-
-
-# In[56]:
-
-
+CREATE TABLE logins (
+        userid integer,
+        tmstmp timestamp, 
+        type varchar(10)
+);
+'''
 cur.execute(query1)
 
-
-# ### Insert csv into new table
-
-# In[57]:
-
+# Insert csv into new table
 query2 = '''
-        COPY logins 
-        FROM '/Users/brad/Dropbox/Galvanize/sql-python/data/lecture-example/logins01.csv' 
-        DELIMITER ',' 
-        CSV;
-        '''
-
-
-# In[58]:
-
-
+COPY logins 
+FROM '/Users/brad/Dropbox/Galvanize/sql-python/data/lecture-example/logins01.csv' 
+DELIMITER ',' 
+CSV;
+'''
 cur.execute(query2)
 
 
-# ### Lets take a look at the data
-
-# In[59]:
-
+# Lets take a look at the data
 query3 = '''
-        SELECT *
-        FROM logins
-        LIMIT 20;
-        '''
-
-
-# In[60]:
-
+SELECT *
+FROM logins
+LIMIT 20;
+'''
 cur.execute(query3)
 
-
-# ### One line at a time
-
-# In[61]:
-
+# One line at a time
 cur.fetchone()
 
-
-# ### Many lines at a time
-
-# In[62]:
-
+# Many lines at a time
 cur.fetchmany(5)
 
-
-# ### Or everything at once
-
-# In[63]:
-
+# Or everything at once
 cur.fetchall()
-
-
-# In[64]:
-
 
 cur.execute('SELECT Count(*) FROM logins')
-
-
-# In[65]:
-
 cur.fetchall()
 
+# Dynamic Queries
+# We have 8 login csv files that we need to insert into the logins table
+# Instead of doing a COPY FROM query 8 times, we should utilize Python to make this more efficient
+# This is possible due to tokenized strings
 
-# # Dynamic Queries
-# 
-# We have 8 login csv files that we need to insert into the logins table.  Instead of doing a COPY FROM query 8 times, we should utilize Python to make this more efficient.  This is possible due to tokenized strings.
-
-# In[47]:
-
-# os is needed because we want to dynamically identify the files 
-# we need to insert.
+# os is needed because we want to dynamically identify the files we need to insert
 import os
 
-
-# In[48]:
-
 query4 = '''
-        COPY logins 
-        FROM %(file_path)s
-        DELIMITER ',' 
-        CSV;
-        '''
-
+COPY logins 
+FROM %(file_path)s
+DELIMITER ',' 
+CSV;
+'''
 folder_path = '/Users/brad/Dropbox/Galvanize/sql-python/data/lecture-example/'
-
-
-# In[49]:
-
 fnames = os.listdir(folder_path)
 
 for fname in fnames:
-    path = os.path.join(folder_path, fname)
-    cur.execute(query4, {'file_path': path})
+        path = os.path.join(folder_path, fname)
+        cur.execute(query4, {'file_path': path})
 
-
-
-# # WARNING: BEWARE OF SQL INJECTION
-# 
-# ## NEVER use + or % to reformat strings to be used with .execute
-
-# In[66]:
-
+# WARNING: BEWARE OF SQL INJECTION
+# NEVER use + or % to reformat strings to be used with .execute
 num = 579
 terribly_unsafe = "SELECT * FROM logins WHERE userid = " + str(num)
-print terribly_unsafe
-
+print(terribly_unsafe)
 
 date_cut = "2014-08-01"
 horribly_risky = "SELECT * FROM logins WHERE tmstmp > %s" % date_cut
-print horribly_risky
+print(horribly_risky)
 ## Python is happy, but if num or date_cut included something malicious
 ## your data could be at risk
 
-
-# ### Don't forget to commit your changes
-
-# In[ ]:
-
+# Don't forget to commit your changes
 cur.commit()
 
-
-# ## And then close your connection
-
-# In[67]:
-
+# And then close your connection
 cur.close()
 conn.close()
 
-
-# # Key Things to Know
+# Key Things to Know
 # 
 # * Connections must be established using an existing database, username, database IP/URL, and maybe passwords
 # * If you have no existing databases, you can connect to Postgres using the dbname 'postgres' to initialize one
@@ -272,12 +152,6 @@ conn.close()
 #     - because it is iterative, previously fetched rows can only be fetched again by rerunning the query
 #     - close cursor through .close()
 # * Cursors and Connections must be closed using .close() or else Postgres will lock certain operations on the database/tables until the connection is severed. 
-# 
 
 # ## And don't leave yourself vulnerable to SQL injection!
 # http://xkcd.com/327/
-
-# In[ ]:
-
-
-
